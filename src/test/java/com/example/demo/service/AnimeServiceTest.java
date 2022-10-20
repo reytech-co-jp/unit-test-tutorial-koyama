4783a075158c355c5a69f21f49c09bc50966ee1e
小山由人 <koyama@reytech.co.jp>
2022-10-20 23:26:42 +0900

    test: Serviceのテストを強化

--- src/test/java/com/example/demo/service/AnimeServiceTest.java
+++ src/test/java/com/example/demo/service/AnimeServiceTest.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.Anime;
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.mapper.AnimeMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,8 +10,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.doReturn;
 
 @ExtendWith(MockitoExtension.class)
@@ -27,6 +30,21 @@ class AnimeServiceTest {
     doReturn(List.of(new Anime(1, "abc", "def"), new Anime(2, "ghi", "jkl"))).when(animeMapper).findAll();
     List<Anime> actual = animeService.getAllAnime();
     assertThat(actual).isEqualTo(List.of(new Anime(1, "abc", "def"), new Anime(2, "ghi", "jkl")));
+  }
+
+  @Test
+  public void アニメが取得できるときに1件アニメを返すこと() {
+    doReturn(Optional.of(new Anime(1, "abc", "def"))).when(animeMapper).findById(1);
+    Anime actual = animeService.getAnime(1);
+    assertThat(actual).isEqualTo(new Anime(1, "abc", "def"));
+  }
+
+  @Test
+  public void アニメが取得できないときに例外をthrowすること() {
+    doReturn(Optional.empty()).when(animeMapper).findById(1);
+    assertThatThrownBy(() -> animeService.getAnime(1))
+        .isInstanceOf(ResourceNotFoundException.class)
+        .hasMessage("resource not found");
   }
 
 }
